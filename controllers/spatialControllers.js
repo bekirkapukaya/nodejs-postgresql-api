a3154df7f131fa4ef93b1a0231a6629e1e7f4a1c
Bekir Kapukaya <bekirkapukaya@gmail.com>
2021-03-29 16:12:24 +0300

    Spatial controllers oluşturuldu

--- controllers/spatialControllers.js
+++ controllers/spatialControllers.js
@@ -1,12 +1,54 @@
 import { Client } from 'pg';
+import GeoJSON from 'geojson';
 
 export const getPoints = async (req, res) => {
+  const { QUERY_POINTS } = process.env;
+
   const client = new Client();
   await client.connect();
-  const data = await client.query('Select Now()');
+  const { rows } = await client.query(QUERY_POINTS);
   await client.end();
 
+  let geojsonData = GeoJSON.parse(rows, {
+    Point: 'data',
+    include: ['name'],
+  });
+
   res.status(200).json({
-    data: data,
+    geojsonData,
+  });
+};
+
+export const getLines = async (req, res) => {
+  const { QUERY_LINES } = process.env;
+  const client = new Client();
+  await client.connect();
+  const { rows } = await client.query(QUERY_LINES);
+  await client.end();
+
+  let geojsonData = GeoJSON.parse(rows, {
+    MultiLineString: 'data',
+    include: ['name'],
+  });
+
+  res.status(200).json({
+    geojsonData,
+  });
+};
+
+export const getPolygons = async (req, res) => {
+  const { QUERY_POLYGONS } = process.env;
+  const client = new Client();
+  await client.connect();
+  const { rows } = await client.query(QUERY_POLYGONS);
+  await client.end();
+
+  let geojsonData = GeoJSON.parse(rows, {
+    MultiPolygon: 'data',
+    include: ['name'],
+  });
+
+  res.status(200).json({
+    geojsonData,
   });
 };
--- routes/spatialRoutes.js
+++ routes/spatialRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { getPoints } from '../controllers/spatialControllers';
+import {
+  getPoints,
+  getLines,
+  getPolygons,
+} from '../controllers/spatialControllers';
 
 const spatialRoutes = Router();
 
 spatialRoutes.get('/getpoints', getPoints);
+spatialRoutes.get('/getlines', getLines);
+spatialRoutes.get('/getpolygons', getPolygons);
 
 export default spatialRoutes;
